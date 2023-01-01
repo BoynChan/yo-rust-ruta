d0191ddfd17838b35ed75b6ba7e5d2b4bb247bbf
BoynChen <chankaser@gmail.com>
2023-01-01 11:08:16 +0800

    add gui library and format usage

--- google-project/src/main.rs
+++ google-project/src/main.rs
@@ -1,8 +1,16 @@
+mod gui_library;
 mod health_statistic;
 mod library;
 mod luhn_alogorithm;
 mod polygon;
-mod router_prefix;
+
+use gui_library::{Button, Label, Widget, Window};
 fn main() {
-    println!("Hello, world!");
+    let mut window = Window::new("Rust GUI Demo 1.23");
+    window.add_widget(Box::new(Label::new("This is a small text GUI demo.")));
+    window.add_widget(Box::new(Button::new(
+        "Click me!",
+        Box::new(|| println!("You clicked the button!")),
+    )));
+    window.draw();
 }
