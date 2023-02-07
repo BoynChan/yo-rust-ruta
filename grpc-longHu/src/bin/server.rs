7f9c8fe0181561c8af59fdcee5f9594ee1899aab
BoynChen <chankaser@gmail.com>
2023-02-07 23:07:15 +0800

    feat: add database connection

--- grpc-longHu/src/bin/server.rs
+++ grpc-longHu/src/bin/server.rs
@@ -1,11 +1,15 @@
-use tonic::{transport::Server, Request, Response, Status};
+use sea_orm::{ActiveModelTrait, EntityTrait, QueryFilter};
+use tonic::{transport::Server, Code, Request, Response, Status};
 
-use hello_world::greeter_server::{Greeter, GreeterServer};
+use greeter_server::{Greeter, GreeterServer};
+use sea_orm::ColumnTrait;
 
-use hello_world::{HelloReply, HelloRequest};
-pub mod hello_world {
-    tonic::include_proto!("helloworld");
-}
+use grpc_longHu::name_table;
+use grpc_longHu::name_table::Entity as Name;
+
+use grpc_longHu::DB;
+
+tonic::include_proto!("helloworld");
 
 #[derive(Debug, Default)]
 pub struct MyGreeter {}
@@ -17,11 +21,47 @@ impl Greeter for MyGreeter {
         request: Request<HelloRequest>,
     ) -> Result<Response<HelloReply>, Status> {
         println!("Got a request: {:?}", request);
-        let reply = hello_world::HelloReply {
+        let reply = HelloReply {
             message: format!("Hello {}", request.into_inner().name).into(),
         };
 
         Ok(Response::new(reply))
+    }
+
+    async fn insert_name(
+        &self,
+        request: Request<InsertNameRequest>,
+    ) -> Result<Response<InsertNameResponse>, Status> {
+        let new_name = name_table::ActiveModel {
+            id: sea_orm::ActiveValue::NotSet,
+            name: sea_orm::ActiveValue::Set(request.get_ref().name.clone()),
+        };
+        if let Err(r) = new_name.insert(DB.get().await).await {
+            return Ok(Response::new(InsertNameResponse { success: false }));
+        }
+        Ok(Response::new(InsertNameResponse { success: true }))
+    }
+
+    async fn get_name(
+        &self,
+        request: Request<GetNameRequest>,
+    ) -> Result<Response<GetNameResponse>, Status> {
+        let result = Name::find()
+            .filter(name_table::Column::Name.eq(request.get_ref().name.as_str()))
+            .one(DB.get().await)
+            .await;
+        if result.is_err() {
+            return Err(Status::new(Code::Unavailable, "db error"));
+        }
+        let find_name = result.unwrap();
+        if find_name.is_none() {
+            return Err(Status::new(Code::NotFound, "name not found"));
+        }
+        let one = find_name.unwrap();
+        Ok(Response::new(GetNameResponse {
+            id: one.id,
+            name: one.name,
+        }))
     }
 }
 
