d05171519193743322bc26c06f50ca4ca0da4880
BoynChen <chankaser@gmail.com>
2023-03-01 23:40:35 +0800

    feat: use shard mutex, calculate hash key by hand

--- async_book/src/main.rs
+++ async_book/src/main.rs
@@ -1,5 +1,6 @@
 use std::{
-    collections::HashMap,
+    collections::{hash_map::DefaultHasher, HashMap},
+    hash::{self, Hash, Hasher},
     sync::{Arc, Mutex},
 };
 
@@ -7,10 +8,11 @@ use bytes::Bytes;
 use mini_redis::{Connection, Frame};
 use tokio::net::{TcpListener, TcpStream};
 
+const NUM_SHARDS: usize = 10;
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-    let db = Arc::new(Mutex::new(HashMap::new()));
+    let db = new_sharded_db(NUM_SHARDS);
     loop {
         let (socket, _) = listener.accept().await.unwrap();
         let db = db.clone();
@@ -21,23 +23,41 @@ async fn main() {
 }
 
 type DB = Arc<Mutex<HashMap<String, Bytes>>>;
+type ShardedDB = Arc<Vec<Mutex<HashMap<String, Bytes>>>>;
 
-async fn process(socket: TcpStream, db: DB) {
+fn new_sharded_db(num_shards: usize) -> ShardedDB {
+    let mut db = Vec::with_capacity(num_shards);
+    for _ in 0..num_shards {
+        db.push(Mutex::new(HashMap::new()));
+    }
+    Arc::new(db)
+}
+
+async fn process(socket: TcpStream, db: ShardedDB) {
     use mini_redis::Command::{self, Get, Set};
     let mut conn = Connection::new(socket);
+
+    let hash_index = |key: String| -> usize {
+        let mut s = DefaultHasher::new();
+        key.hash(&mut s);
+        s.finish() as usize % NUM_SHARDS
+    };
 
     while let Some(frame) = conn.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
                 println!("Set:{:?}", cmd);
-                db.lock()
+                let key = cmd.key().to_string();
+                db[hash_index(key.clone())]
+                    .lock()
                     .unwrap()
-                    .insert(cmd.key().to_string(), cmd.value().clone());
+                    .insert(key, cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
                 println!("Get:{:?}", cmd);
-                if let Some(value) = db.lock().unwrap().get(cmd.key()) {
+                let key = cmd.key().to_string();
+                if let Some(value) = db[hash_index(key.clone())].lock().unwrap().get(cmd.key()) {
                     Frame::Bulk(value.clone().into())
                 } else {
                     Frame::Null
